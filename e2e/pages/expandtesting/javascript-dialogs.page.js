92dc4733a3e7b0af0a49817b3c8302ef1474fe0e
Michal Gaworski <tapirek@gmail.com>
2026-01-22 11:08:34 +0100

    JS dialogs (alert, confirm, prompt) tests.

---
+++ e2e/pages/expandtesting/javascript-dialogs.page.js
@@ -0,0 +1,43 @@
+import { BasePage } from "../base.page";
+
+export class JavaScriptDialogsPage extends BasePage {
+
+    constructor(page, cfg = {}) {
+        super(page, cfg);
+        this.url = `${this.cfg.appURL}js-dialogs`;
+    }
+
+    // locators
+    get jsAlertButton() { return this.page.getByRole("button").getByText("Js Alert"); };
+    get jsConfirmButton() { return this.page.getByRole("button").getByText("Js Confirm"); };
+    get jsPromptButton() { return this.page.getByRole("button").getByText("Js Prompt"); };
+    get dialogResponseText() { return this.page.locator("#dialog-response"); };
+
+    // methods
+    async alert() {
+        this.page.on('dialog', dialog => dialog.accept());
+        await this.jsAlertButton.click();
+    }
+
+    async confirmCancel() {
+        this.page.on('dialog', dialog => dialog.dismiss());
+        await this.jsConfirmButton.click();
+    }
+
+    async confirmAccept() {
+        this.page.on('dialog', dialog => dialog.accept());
+        await this.jsConfirmButton.click();
+    }
+
+    async promptCancel() {
+        this.page.on('dialog', dialog => dialog.dismiss());
+        await this.jsPromptButton.click();
+    }
+
+    async promptEnter(prompt) {
+        this.page.on('dialog', dialog => dialog.accept(prompt));
+        await this.jsPromptButton.click();
+    }
+    
+
+}
---
+++ e2e/tests/expandtesting/expandtesting-020-js-dialogs.spec.js
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+import { JavaScriptDialogsPage } from '../../pages/expandtesting/javascript-dialogs.page';
+
+const cfg = require('../../utilities/loadEnvHelper');
+
+test.describe("Expand testing #20 - JS Dialogs", () => {
+
+    test.beforeEach(async ({ page }) => {
+        const javaScriptDialogsPage = new JavaScriptDialogsPage(page, cfg);
+        await javaScriptDialogsPage.open();
+    });
+
+    // Alert
+    test("Verify JS Alert can be accepted", async ({ page }) => {
+        const javaScriptDialogsPage = new JavaScriptDialogsPage(page, cfg);
+        await javaScriptDialogsPage.alert();
+        await expect(javaScriptDialogsPage.dialogResponseText).toHaveText("OK");
+    });
+
+    // Confirm
+    test("Verify JS Confirm can be canceled", async ({ page }) => {
+        const javaScriptDialogsPage = new JavaScriptDialogsPage(page, cfg);
+        await javaScriptDialogsPage.confirmCancel();
+        await expect(javaScriptDialogsPage.dialogResponseText).toHaveText("Cancel");
+    });
+
+    test("Verify JS Confirm can be accepted", async ({ page }) => {
+        const javaScriptDialogsPage = new JavaScriptDialogsPage(page, cfg);
+        await javaScriptDialogsPage.confirmAccept();
+        await expect(javaScriptDialogsPage.dialogResponseText).toHaveText("Ok");
+    });
+
+    // Prompt
+    test("Verify JS Prompt can be canceled", async ({ page }) => {
+        const javaScriptDialogsPage = new JavaScriptDialogsPage(page, cfg);
+        await javaScriptDialogsPage.promptCancel();
+        await expect(javaScriptDialogsPage.dialogResponseText).toHaveText("");
+    });
+
+    test("Verify JS Prompt can be entered and accepted", async ({ page }) => {
+        const PROMPT = "This is some prompt I just made up on the spot!";
+        const javaScriptDialogsPage = new JavaScriptDialogsPage(page, cfg);
+        await javaScriptDialogsPage.promptEnter(PROMPT);
+        await expect(javaScriptDialogsPage.dialogResponseText).toHaveText(PROMPT);
+    });
+
+});
