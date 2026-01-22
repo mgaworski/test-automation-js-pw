4c514f07e820e364cf0209a78ea92909b0981a1d
Michal Gaworski <tapirek@gmail.com>
2026-01-22 14:49:36 +0100

    Fixing missplaced resource.

--- tests/e2e/tests/expandtesting/expandtesting-014-file-upload.spec.js
+++ tests/e2e/tests/expandtesting/expandtesting-014-file-upload.spec.js
@@ -10,7 +10,7 @@ const cfg = require('../../../utilities/loadEnvHelper');
 test.describe("Expand testing #14 - File upload", () => {
 
     const FILE_NAME = "file_to_be_uploaded.txt";
-    const FILE_PATH = `./tests/e2e/resources/${FILE_NAME}`;
+    const FILE_PATH = `./tests/resources/${FILE_NAME}`;
 
     test("Verify file upload", async ({ page }) => {
         const fileUploaderPage = new FileUploaderPage(page, cfg);
