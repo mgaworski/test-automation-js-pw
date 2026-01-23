61a6192a1c969ebe9308e2e87461875fca58c6e6
Michal Gaworski <tapirek@gmail.com>
2026-01-23 11:33:37 +0100

    IPify sample test adjusted.

--- e2e/tests-api/tests/sample-api-test.spec.js
+++ e2e/tests-api/tests/sample-api-test.spec.js
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 const cfg = require('../../utilities/loadEnvHelper');
 
 test('GET my ip', async ({ request }) => {
-    const res = await request.get(cfg.apiURL);
+    const res = await request.get(cfg.ipifyURL);
     expect(res.ok()).toBeTruthy();
 });
